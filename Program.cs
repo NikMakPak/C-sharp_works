4bfb52204f253fe05e020bd61ad82d36d42db01f
nikmak <57749266+NikMakPak@users.noreply.github.com>
2021-12-13 13:24:36 +0300

    result finish

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,39 +6,61 @@ using System.Threading.Tasks;
 
 namespace shop_nikmak
 {
-    struct WPriceCorp
+    struct WPrice
     {
-        public double sum10, sum100, sum1000;
+        public double wclient,wcorp;
 
-        public WPriceCorp(int sum10, int sum100, int sum1000)
+        public WPrice(double wclient, double wcorp)
         {
-            this.sum10 = sum10;
-            this.sum100 = sum100;
-            this.sum1000 = sum1000;
+            this.wclient = wclient;
+            this.wcorp = wcorp;
         }
-        public void Print()
-        {
-            Console.WriteLine("\t\t\t\t\tКорпоративная");
-        }
-    }
-    struct WPriceClient
-    {
-        public double sum10, sum100, sum1000;
 
-        public WPriceClient(int sum10, int sum100, int sum1000)
-        {
-            this.sum10 = sum10;
-            this.sum100 = sum100;
-            this.sum1000 = sum1000;
-        }
         public void Print()
         {
             Console.WriteLine("\t\t\t\tОптовая:\n\t\t\t\t\tКлиентская:");
+            Console.WriteLine($"\t\t\t\t\t\t10: {wclient*0.98}\n\t\t\t\t\t\t100: {wclient*0.97}\n\t\t\t\t\t\t1000: {wclient*0.95}");
+            Console.WriteLine("\t\t\t\t\tКорпоративная:");
+            Console.WriteLine($"\t\t\t\t\t\t10: {wcorp * 0.98}\n\t\t\t\t\t\t100: {wcorp * 0.97}\n\t\t\t\t\t\t1000: {wcorp * 0.95}");
+        }
+        public void Cost(string tip, string disc)
+        {
+            switch (tip)
+            {
+                case "базовый":
+                    wclient *= 0.95;
+                    wcorp *= 0.95;
+                    break;
+                case "серебро":
+                    wclient *= 0.90;
+                    wcorp *= 0.90;
+                    break;
+                case "золото":
+                    wclient *= 0.85;
+                    wcorp *= 0.85;
+                    break;
+            }
+
+            switch (disc)
+            {
+                case "эконом":
+                    wclient *= 0.94;
+                    wcorp *= 0.97;
+                    break;
+                case "стандарт":
+                    wclient *= 0.93;
+                    wcorp *= 0.95;
+                    break;
+                case "премиум":
+                    wclient *= 0.89;
+                    wcorp *= 0.94;
+                    break;
+            }
         }
     }
     struct BPrice
     {
-        public double client,corp;
+        public double client, corp;
 
         public BPrice(int client, int corp)
         {
@@ -46,38 +68,38 @@ namespace shop_nikmak
             this.corp = corp;
         }
         public void Cost(string tip, string disc)
+        {
+            switch (tip)
             {
-                switch (tip)
-                {
-                    case "базовый":
-                        client *= 0.95;
-                        corp *= 0.95;
-                        break;
-                    case "серебро":
-                        client *= 0.90;
-                        corp *= 0.90;
-                        break;
-                    case "золото":
-                        client *= 0.85;
-                        corp *= 0.85;
-                        break;
-                }
+                case "базовый":
+                    client *= 0.95;
+                    corp *= 0.95;
+                    break;
+                case "серебро":
+                    client *= 0.90;
+                    corp *= 0.90;
+                    break;
+                case "золото":
+                    client *= 0.85;
+                    corp *= 0.85;
+                    break;
+            }
 
-                switch (disc)
-                {
-                    case "эконом":
-                        client *= 0.95;
-                        corp *= 0.98;
-                        break;
-                    case "стандарт":
-                        client *= 0.93;
-                        corp *= 0.96;
-                        break;
-                    case "премиум":
-                        client *= 0.90;
-                        corp *= 0.95;
-                        break;
-                }
+            switch (disc)
+            {
+                case "эконом":
+                    client *= 0.95;
+                    corp *= 0.98;
+                    break;
+                case "стандарт":
+                    client *= 0.93;
+                    corp *= 0.96;
+                    break;
+                case "премиум":
+                    client *= 0.90;
+                    corp *= 0.95;
+                    break;
+            }
         }
         public void Print()
         {
@@ -85,10 +107,10 @@ namespace shop_nikmak
         }
 
     }
-    
+
     struct Parameter
     {
-        public string hieght,wieght,length,quantity,shelfLife,color;
+        public string hieght, wieght, length, quantity, shelfLife, color;
 
         public Parameter(string hieght = null, string wieght = null, string length = null, string quantity = null, string shelfLife = null, string color = null)
         {
@@ -107,7 +129,7 @@ namespace shop_nikmak
     }
     struct Product
     {
-        public string title,typi,discountClass;
+        public string title, typi, discountClass;
 
         public Product(string title, string typi, string discountClass)
         {
@@ -146,7 +168,7 @@ namespace shop_nikmak
 
         public void Print()
         {
-            Console.WriteLine("Имя магазина: {0}\n\tТип лояльности: {1}",name, loyalty);
+            Console.WriteLine("Имя магазина: {0}\n\tТип лояльности: {1}", name, loyalty);
         }
     }
 
@@ -154,22 +176,35 @@ namespace shop_nikmak
     {
         static void Main(string[] args)
         {
-            WPriceCorp optcorp1 = new WPriceCorp(880,820,760);
-            WPriceClient optcli1 = new WPriceClient(850,800,750);
-            BPrice price1 = new BPrice(1050,850);
-            Parameter param1 = new Parameter("120 см", "1 кг", "50 см", "1 шт", "2 года","красный");
+            WPrice opt2 = new WPrice(93000, 95500);
+            BPrice price2 = new BPrice(98000, 99990);
+            Parameter param2 = new Parameter("6.4``", "107 гр", "1 см", "1 шт", "5 лет", "красный");
+            Product prod2 = new Product("Смартфон Бэпл", "Электроника", "премиум");
+            Stand st2 = new Stand("2");
+            WPrice opt1 = new WPrice(850, 880);
+            BPrice price1 = new BPrice(1050, 850);
+            Parameter param1 = new Parameter("120 см", "1 кг", "50 см", "1 шт", "2 года", "белый");
             Product prod1 = new Product("Чайник", "Бытовая техника", "эконом");
             Stand st1 = new Stand("1");
-            Shop shop1 = new Shop("рога и копыта","серебро");
+            Shop shop1 = new Shop("рога и копыта", "серебро");
             shop1.Print();
             st1.Print();
             prod1.Print();
             param1.Print();
-
-            price1.Cost(shop1.loyalty,prod1.discountClass);
+            price1.Cost(shop1.loyalty, prod1.discountClass);
             price1.Print();
+            opt1.Cost(shop1.loyalty, prod1.discountClass);
+            opt1.Print();
 
-            //оптом
+            //2 стенд
+
+            st2.Print();
+            prod2.Print();
+            param2.Print();
+            price2.Cost(shop1.loyalty, prod1.discountClass);
+            price2.Print();
+            opt2.Cost(shop1.loyalty, prod1.discountClass);
+            opt2.Print();
 
             Console.ReadKey();
         }
