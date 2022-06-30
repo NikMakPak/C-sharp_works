1d9578a0e29cbcf0080721a530dbb29cc26b8ee0
nikmak <57749266+NikMakPak@users.noreply.github.com>
2022-06-30 21:18:01 +0300

    init project
    Make an private card system with user and his card. With get set and oop

--- Program.cs
+++ Program.cs
@@ -8,167 +8,90 @@ namespace oop_training
 {
     internal class Program
     {
-        abstract public class Global
+        class Global
         {
-            static public string[] colors = { "белого", "желтого","синего", "красного", "оранжегого", "зеленого", "голубого", "фиолетогого", "серого", "прозрачного" };
-            static public Random rand = new Random((int)DateTime.Now.Ticks);
-            static public string[] types = { "жирный котэ", "сибирский", "уральский", "домашний" };
-            static public MutantCat goFunnel(List<CatProt> cats)
+            private static List<int> ids = new List<int>();
+            public Random rand = new Random((int)DateTime.Now.Ticks);
+            public string[] alph = { "а", "б", "в", "и", "у", "ж", "н", "я", };
+            public string genName()
             {
-                string color = "{ ";
-                string type = "{ ";
-                List<Skill> abilities = new List<Skill> { };
-                for (int i = 0; i < cats.Count; i++)
+                string name = "";
+                for (int i = 0; i < rand.Next(3,10); i++)
                 {
-                    if (cats[i] is Cat cat)
+                    name += alph[rand.Next(alph.Length)];
+                }
+                return name;
+            }
+            public int genId()
+            {
+                bool f = false;
+                while (f!=true)
+                {
+                    int id = rand.Next(100000, 1000000);
+                    if (!ids.Contains(id))
                     {
-                        color += cat.color + ", ";
-                        type += cat.type + ", ";
-                        foreach (Skill skill in cat.abilities)
-                        {
-                            abilities.Add(skill);
-                        }
-                    }
-                    else if (cats[i] is MutantCat mCat)
-                    {
-                        color += mCat.color + ", ";
-                        type += mCat.type + ", ";
-                        foreach (Skill skill in mCat.abilities)
-                        {
-                            abilities.Add(skill);
-                        }
+                       f= true;
+                       ids.Add(id);
+                       return id;
                     }
                 }
-                color += " }";
-                type += " }";
-                return new MutantCat(color, type, abilities);
+                return 0;
             }
         }
-        public class Skill : Global
+        class User : Global
         {
-            string skill;
+            public int id { get; }
+            public string name { get; }
+            public Card userCard { get; set; }
 
-            public Skill()
+            public User()
             {
-                this.skill = genSkill();
-            }
-
-            public void printInfo()
-            {
-                Console.WriteLine($"- {skill}");
-            }
-            string genSkill()
-            {
-                return $"ловить {rand.Next(201)} бабочек {colors[rand.Next(201) % (colors.Length)]} цвета";
-            }
-        }
-
-        abstract public class CatProt : Global
-        {
-            public string color, type;
-            public int weight;
-            public List<Skill> abilities = new List<Skill> ();
-
-            public void play()
-            {
-                Console.WriteLine("Я играю");
-            }
-            public void mya()
-            {
-                Console.WriteLine("Я мяукаю");
+                id = genId();
+                name = genName();
             }
             public void tellAbout()
             {
-                Console.WriteLine($"Я кот типа {type}; {color} цвета; с весом {weight} кг");
-                Console.WriteLine("Я умею:");
-                if (abilities.Count>0)
-                {
-                    foreach (Skill skill in abilities)
-                    {
-                        skill.printInfo();
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("- пока ничего :(");
-                }
+                Console.WriteLine($"Меня зовут {name}. Мой id: {id}. Я владею картой: {userCard.card4numbers}");
             }
-            public void trainSkill()
+            public void makeCard()
             {
-                if (rand.Next(10)>5)
-                {
-                    abilities.Add(new Skill());
-                }
+                Console.Write("Введите пин-код для вашей новой карты:");
+                userCard = new Card(this,Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("Карта успешно выпущена!");
             }
         }
-
-        public class Cat : CatProt
+        class Card : Global
         {
-            public Cat()
+            public User cardHolder { get; }
+            public int card4numbers { get; }
+            public int pin { get; set; }
+
+            public Card(User cardHolder, int pin)
             {
-                this.color = colors[rand.Next(100) % (colors.Length)];
-                this.type = types[rand.Next(100) % (types.Length)];
-                this.weight = rand.Next(10, 51);
-                trainSkill();
+                this.cardHolder = cardHolder;
+                this.card4numbers = rand.Next(1000,10000);
+                this.pin = pin;
+            }
+
+            public void tellAbout()
+            {
+                Console.WriteLine($"Инфо о карте:\n\t-Хозяин: {cardHolder.name} - {cardHolder.id}\n\t-4 цифры: {card4numbers}\n\t-пин-код: {pin}");
             }
         }
-
-        public class MutantCat : CatProt
-        {
-            public MutantCat(string color, string type, List<Skill> abilities)
-            {
-                this.color = color;
-                this.type = type;
-                this.weight = rand.Next(10, 51);
-                this.abilities = abilities;
-            }
-        }
-
         static void Main(string[] args)
         {
-            
-            action(10);
-            action(100);
-            action(1000);
-
-
-            void action(int maxRange)
+            List<User> users = new List<User>();
+            for (int i = 0; i < 5; i++)
             {
-                // создание кучи котов
-                List<CatProt> catsHeap = new List<CatProt> { };
-                for (int i = 0; i < maxRange + 10; i++)
-                {
-                    catsHeap.Add(new Cat());
-                }
-
-                int mixCount = 0;
-                while (mixCount < maxRange)
-                {
-                    // создание группы котов в воронку
-                    int range = Global.rand.Next(1, catsHeap.Count / 2);
-                    int index = Global.rand.Next((catsHeap.Count - range < 0) ? 0 : catsHeap.Count - range);
-                    //int range = 2;
-                    //int index = catsHeap.Count - 2;
-                    List<CatProt> group = catsHeap.GetRange(index, range);
-                    catsHeap.RemoveRange(index, range);
-                    mixCount += range;
-
-                    // отправка и принятие в воронку
-                    catsHeap.Add(Global.goFunnel(group));
-                }
-                
-                Console.WriteLine($"\n#####\nПри смешивании {maxRange} кошек:");
-                int n = 0;
-                foreach (MutantCat mut in catsHeap.FindAll(x => x.GetType().Name == "MutantCat"))
-                {
-                    n++;
-                    Console.WriteLine($"\n\t#{n})");
-                    mut.tellAbout();
-                }
-
+                users.Add(new User());
+                users[i].makeCard();
             }
-            
+            foreach (var u in users)
+            {
+                Console.WriteLine("\n");
+                u.tellAbout();
+                u.userCard.tellAbout();
+            }
         }
-            
     }
 }
