31bbce354518313e9857cc50ca802b4bfada40a3
nikmak <57749266+NikMakPak@users.noreply.github.com>
2022-06-30 12:39:17 +0300

    init new project
    Made for task: https://sun9-65.userapi.com/impf/8fy4n8n8xT5hwtFPguZ5mpb_Hhkt9s3L55EYfA/v3Xl7FyRhog.jpg?size=1920x1439&quality=95&sign=74d833785cb1ef8b1e120b11ddc1d367&type=album

--- Program.cs
+++ Program.cs
@@ -4,209 +4,172 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace shop_nikmak
+namespace oop_training
 {
-    struct WPrice
+    //todo: понять почему выводит пустые строки
+    internal class Program
     {
-        public double wclient,wcorp;
-
-        public WPrice(double wclient, double wcorp)
+        abstract public class Global
         {
-            this.wclient = wclient;
-            this.wcorp = wcorp;
-        }
-
-        public void Print()
-        {
-            Console.WriteLine("\t\t\t\tОптовая:\n\t\t\t\t\tКлиентская:");
-            Console.WriteLine($"\t\t\t\t\t\t10: {wclient*0.98}\n\t\t\t\t\t\t100: {wclient*0.97}\n\t\t\t\t\t\t1000: {wclient*0.95}");
-            Console.WriteLine("\t\t\t\t\tКорпоративная:");
-            Console.WriteLine($"\t\t\t\t\t\t10: {wcorp * 0.98}\n\t\t\t\t\t\t100: {wcorp * 0.97}\n\t\t\t\t\t\t1000: {wcorp * 0.95}");
-        }
-        public void Cost(string tip, string disc)
-        {
-            switch (tip)
+            static public string[] colors = { "белого", "желтого","синего", "красного", "оранжегого", "зеленого", "голубого", "фиолетогого", "серого", "прозрачного" };
+            static public Random rand = new Random((int)DateTime.Now.Ticks);
+            static public string[] types = { "жирный котэ", "сибирский", "уральский", "домашний" };
+            static public MutantCat goFunnel(List<CatProt> cats)
             {
-                case "базовый":
-                    wclient *= 0.95;
-                    wcorp *= 0.95;
-                    break;
-                case "серебро":
-                    wclient *= 0.90;
-                    wcorp *= 0.90;
-                    break;
-                case "золото":
-                    wclient *= 0.85;
-                    wcorp *= 0.85;
-                    break;
-            }
-
-            switch (disc)
-            {
-                case "эконом":
-                    wclient *= 0.94;
-                    wcorp *= 0.97;
-                    break;
-                case "стандарт":
-                    wclient *= 0.93;
-                    wcorp *= 0.95;
-                    break;
-                case "премиум":
-                    wclient *= 0.89;
-                    wcorp *= 0.94;
-                    break;
+                string color = "{ ";
+                string type = "{ ";
+                List<Skill> abilities = new List<Skill> { };
+                for (int i = 0; i < cats.Count; i++)
+                {
+                    if (cats[i] is Cat cat)
+                    {
+                        color += cat.color + ", ";
+                        type += cat.type + ", ";
+                        foreach (Skill skill in cat.abilities)
+                        {
+                            abilities.Add(skill);
+                        }
+                    }
+                    else if (cats[i] is MutantCat mCat)
+                    {
+                        color += mCat.color + ", ";
+                        type += mCat.type + ", ";
+                        foreach (Skill skill in mCat.abilities)
+                        {
+                            abilities.Add(skill);
+                        }
+                    }
+                }
+                color += " }";
+                type += " }";
+                return new MutantCat(color, type, abilities);
             }
         }
-    }
-    struct BPrice
-    {
-        public double client, corp;
+        public class Skill : Global
+        {
+            string skill;
 
-        public BPrice(int client, int corp)
-        {
-            this.client = client;
-            this.corp = corp;
-        }
-        public void Cost(string tip, string disc)
-        {
-            switch (tip)
+            public Skill()
             {
-                case "базовый":
-                    client *= 0.95;
-                    corp *= 0.95;
-                    break;
-                case "серебро":
-                    client *= 0.90;
-                    corp *= 0.90;
-                    break;
-                case "золото":
-                    client *= 0.85;
-                    corp *= 0.85;
-                    break;
+                this.skill = genSkill();
             }
 
-            switch (disc)
+            public void printInfo()
             {
-                case "эконом":
-                    client *= 0.95;
-                    corp *= 0.98;
-                    break;
-                case "стандарт":
-                    client *= 0.93;
-                    corp *= 0.96;
-                    break;
-                case "премиум":
-                    client *= 0.90;
-                    corp *= 0.95;
-                    break;
+                Console.WriteLine($"- {skill}");
+            }
+            string genSkill()
+            {
+                return $"ловить {rand.Next(201)} бабочек {colors[rand.Next(201) % (colors.Length)]} цвета";
             }
         }
-        public void Print()
+
+        abstract public class CatProt : Global
         {
-            Console.WriteLine("\t\t\tЦена:\n\t\t\t\tБазовая:\n\t\t\t\t\tКлиентская: {0}\n\t\t\t\t\tКорпоративная: {1}", client, corp);
+            public string color, type;
+            public int weight;
+            public List<Skill> abilities = new List<Skill> ();
+
+            public void play()
+            {
+                Console.WriteLine("Я играю");
+            }
+            public void mya()
+            {
+                Console.WriteLine("Я мяукаю");
+            }
+            public void tellAbout()
+            {
+                Console.WriteLine($"Я кот типа {type}; {color} цвета; с весом {weight} кг");
+                Console.WriteLine("Я умею:");
+                if (abilities.Count>0)
+                {
+                    foreach (Skill skill in abilities)
+                    {
+                        skill.printInfo();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("- пока ничего :(");
+                }
+            }
+            public void trainSkill()
+            {
+                if (rand.Next(10)>5)
+                {
+                    abilities.Add(new Skill());
+                }
+            }
         }
 
-    }
-
-    struct Parameter
-    {
-        public string hieght, wieght, length, quantity, shelfLife, color;
-
-        public Parameter(string hieght = null, string wieght = null, string length = null, string quantity = null, string shelfLife = null, string color = null)
+        public class Cat : CatProt
         {
-            this.hieght = hieght;
-            this.wieght = wieght;
-            this.length = length;
-            this.quantity = quantity;
-            this.shelfLife = shelfLife;
-            this.color = color;
-        }
-        public void Print()
-        {
-            Console.WriteLine("\t\t\tПараметры:\n\t\t\t\tВысота: {0}\n\t\t\t\tВес: {1}\n\t\t\t\tДлина: {2}", hieght, wieght, length);
-            Console.WriteLine("\t\t\t\tКол-во: {0}\n\t\t\t\tСрок годности: {1}\n\t\t\t\tЦвет: {2}", quantity, shelfLife, color);
-        }
-    }
-    struct Product
-    {
-        public string title, typi, discountClass;
-
-        public Product(string title, string typi, string discountClass)
-        {
-            this.title = title;
-            this.typi = typi;
-            this.discountClass = discountClass;
-        }
-        public void Print()
-        {
-            Console.WriteLine("\t\tТовар {0}:\n\t\t\tТип: {1}\n\t\t\tКласс: {2}", title, typi, discountClass);
-        }
-    }
-    struct Stand
-    {
-        public string number;
-
-        public Stand(string number)
-        {
-            this.number = number;
-        }
-        public void Print()
-        {
-            Console.WriteLine("\tСтойка {0}:", number);
-
-        }
-    }
-    struct Shop
-    {
-        public string name, loyalty;
-
-        public Shop(string name, string loyalty)
-        {
-            this.name = name;
-            this.loyalty = loyalty;
+            public Cat()
+            {
+                this.color = colors[rand.Next(100) % (colors.Length)];
+                this.type = types[rand.Next(100) % (types.Length)];
+                this.weight = rand.Next(10, 51);
+                trainSkill();
+            }
         }
 
-        public void Print()
+        public class MutantCat : CatProt
         {
-            Console.WriteLine("Имя магазина: {0}\n\tТип лояльности: {1}", name, loyalty);
+            public MutantCat(string color, string type, List<Skill> abilities)
+            {
+                this.color = color;
+                this.type = type;
+                this.weight = rand.Next(10, 51);
+                this.abilities = abilities;
+            }
         }
-    }
 
-    class Program
-    {
         static void Main(string[] args)
         {
-            WPrice opt2 = new WPrice(93000, 95500);
-            BPrice price2 = new BPrice(98000, 99990);
-            Parameter param2 = new Parameter("6.4``", "107 гр", "1 см", "1 шт", "5 лет", "красный");
-            Product prod2 = new Product("Смартфон Бэпл", "Электроника", "премиум");
-            Stand st2 = new Stand("2");
-            WPrice opt1 = new WPrice(850, 880);
-            BPrice price1 = new BPrice(1050, 850);
-            Parameter param1 = new Parameter("120 см", "1 кг", "50 см", "1 шт", "2 года", "белый");
-            Product prod1 = new Product("Чайник", "Бытовая техника", "эконом");
-            Stand st1 = new Stand("1");
-            Shop shop1 = new Shop("рога и копыта", "серебро");
-            shop1.Print();
-            st1.Print();
-            prod1.Print();
-            param1.Print();
-            price1.Cost(shop1.loyalty, prod1.discountClass);
-            price1.Print();
-            opt1.Cost(shop1.loyalty, prod1.discountClass);
-            opt1.Print();
+            
+            action(10);
+            action(100);
+            action(1000);
 
-            //2 стенд
 
-            st2.Print();
-            prod2.Print();
-            param2.Print();
-            price2.Cost(shop1.loyalty, prod1.discountClass);
-            price2.Print();
-            opt2.Cost(shop1.loyalty, prod1.discountClass);
-            opt2.Print();
+            void action(int maxRange)
+            {
+                // создание кучи котов
+                List<CatProt> catsHeap = new List<CatProt> { };
+                for (int i = 0; i < maxRange + 10; i++)
+                {
+                    catsHeap.Add(new Cat());
+                }
 
-            Console.ReadKey();
+                int mixCount = 0;
+                while (mixCount < maxRange)
+                {
+                    // создание группы котов в воронку
+                    int range = Global.rand.Next(1, catsHeap.Count / 2);
+                    int index = Global.rand.Next((catsHeap.Count - range < 0) ? 0 : catsHeap.Count - range);
+                    //int range = 2;
+                    //int index = catsHeap.Count - 2;
+                    List<CatProt> group = catsHeap.GetRange(index, range);
+                    catsHeap.RemoveRange(index, range);
+                    mixCount += range;
+
+                    // отправка и принятие в воронку
+                    catsHeap.Add(Global.goFunnel(group));
+                }
+                
+                Console.WriteLine($"\n#####\nПри смешивании {maxRange} кошек:");
+                int n = 0;
+                foreach (MutantCat mut in catsHeap.FindAll(x => x.GetType().Name == "MutantCat"))
+                {
+                    n++;
+                    Console.WriteLine($"\n\t#{n})");
+                    mut.tellAbout();
+                }
+
+            }
+            
         }
+            
     }
 }
